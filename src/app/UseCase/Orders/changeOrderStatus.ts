67a52747db907f99a8f8ce970de301c27892d377
Nicole Palmira de Oliveira <nicolepalmira1@gmail.com>
2024-12-26 09:47:00 -0300

    Consertando o erro sdk

--- src/app/UseCase/Orders/changeOrderStatus.ts
+++ src/app/UseCase/Orders/changeOrderStatus.ts
@@ -1,55 +1,23 @@
 import { Request, Response } from 'express';
 import { Order } from '../../models/Order';
 
-
-
-export async function changeOrderStatus(req: Request, res: Response) {
+export async function changeOrderStatus(req: Request, res: Response): Promise<void> {
   try {
     const { orderId } = req.params;
-    const { status } =req.body;
+    const { status } = req.body;
 
-    if(!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
-      return res.status(400).json({
-        error: 'Status should  be one of these: Waiting, In_Production ou Done.'
+    if (!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
+      res.status(400).json({
+        error: 'Status should be one of these: WAITING, IN_PRODUCTION, or DONE.',
       });
-    }
-
-    await Order.findByIdAndUpdate(orderId, { status});
-
-    res.sendStatus(204);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-}
-
-
-
-
-
-
-
-/*import { Request, Response } from 'express';
-
-import { Order } from '../../models/Order';
-
-export async function changeOrderStatus(req: Request, res: Response) {
-  try {
-    const { orderId } = req.params;
-    const { status } =req.body;
-
-    if(!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
-      return res.status(400).json({
-        error: 'Status should  be one of these: Waiting, In_Production ou Done.'
-      });
+      return;
     }
 
     await Order.findByIdAndUpdate(orderId, { status });
 
     res.sendStatus(204);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.sendStatus(500);
   }
 }
-*/
